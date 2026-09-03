254c5e424bd36254e1a752ce4fa777d406b410ad
RyoF20 <ryofebriano12345@gmail.com>
2026-09-03 19:33:35 +0700

    Versi1

---
+++ tipedata.dart
@@ -0,0 +1,45 @@
+void main() {
+  //String nama = "Ryo";
+var nama1 = "Ryo"; //var bisa menyesuaikan tipe data apapun mau int ataupun string bisa otomatis menyesuaikan 
+int angka = 100;
+//var angka1 = 100;
+
+String namabaru = 'robet';
+int umur = 19;
+double  berat = 50.4;
+String kota = 'Pontianak';
+print ("nama saya: $namabaru umur saya :$umur, beratbadan :$berat,Kota saya: $kota");
+
+int angkabaru = 20;
+double angkabaru1 = 20.5;
+num hasil = 0;
+hasil = angkabaru + angkabaru1;
+//num, dynamic
+dynamic nama = "ryo";
+dynamic angka1 = 1000;
+
+//bool bangunpagi = true;
+//var bangunpagi = true;
+//dynamic bangunpagi = true;
+
+//array => dart = list,set,map
+
+//List<dynamic> mhs = ['deny', 'arkan', 'cindy', 100, true, 20.5];
+//List<String> mhs1 = ['deny', 'arkan', 'cindy'];
+//print('nama saya ${mhs[0]}');
+//List data = ['deny', 30, 'sistem informasi'];
+//nama saya deny, umur 30 prodi sistem informasi 
+//print('nama saya ${data[0]} ,umur ${data[1]} ,prodi ${data[2]} ');
+
+// List data = [];
+// data.add(100);
+// data.add('ryo');
+// data.add('Sistem Informasi');
+// data.removeAt(1);
+
+// //print (data.elementAt(0));
+// //print (data.indexOf(100));
+// print (data.removeAt(1));
+
+}
+
